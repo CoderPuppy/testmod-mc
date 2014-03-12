705cf665d54eef55733f14efe401ef467823323d
CoderPuppy <cpup@cpup-laptop>
2014-03-12 12:20:02 -0400

    Add the dagger

---
+++ src/main/scala/cpup/mc/testMod/client/DaggerRenderer.scala
@@ -0,0 +1,59 @@
+package cpup.mc.testMod.client
+
+import net.minecraftforge.client.IItemRenderer
+import net.minecraft.item.ItemStack
+import net.minecraftforge.client.IItemRenderer.{ItemRendererHelper, ItemRenderType}
+import cpup.mc.testMod.content.{ItemDagger, ItemSwordBow}
+import net.minecraft.util.IIcon
+import org.lwjgl.opengl.{GL12, GL11}
+import net.minecraft.client.renderer.texture.{TextureManager, TextureUtil}
+import net.minecraft.client.renderer.{ItemRenderer, OpenGlHelper, Tessellator}
+import net.minecraft.client.Minecraft
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.Items
+
+class DaggerRenderer extends IItemRenderer {
+	@Override
+	def handleRenderType(stack: ItemStack, renderType: ItemRenderType) = if(stack.getItem.isInstanceOf[ItemDagger]) {
+		renderType == ItemRenderType.EQUIPPED || renderType == ItemRenderType.EQUIPPED_FIRST_PERSON
+	} else { false }
+
+	@Override
+	def shouldUseRenderHelper(renderType: ItemRenderType, stack: ItemStack, helper: ItemRendererHelper) = false
+
+	@Override
+	def renderItem(renderType: ItemRenderType, stack: ItemStack, data: Object*) {
+		val texManager = Minecraft.getMinecraft.getTextureManager
+		val livingEntity = data(1).asInstanceOf[EntityLivingBase]
+		val item = stack.getItem.asInstanceOf[ItemDagger]
+
+		GL11.glPushMatrix
+
+		val icon = item.getIconIndex(stack)
+		if(icon == null) {
+			GL11.glPopMatrix
+			return
+		}
+
+		texManager.bindTexture(texManager.getResourceLocation(stack.getItemSpriteNumber))
+		TextureUtil.func_147950_a(false, false)
+
+		val tessellator: Tessellator = Tessellator.instance
+
+		val minU: Float = icon.getMinU
+		val maxU: Float = icon.getMaxU
+		val minV: Float = icon.getMinV
+		val maxV: Float = icon.getMaxV
+
+		GL11.glEnable(GL12.GL_RESCALE_NORMAL)
+
+		ItemRenderer.renderItemIn2D(tessellator, maxU, minV, minU, maxV, icon.getIconWidth, icon.getIconHeight, 0.0625F)
+
+		GL11.glDisable(GL12.GL_RESCALE_NORMAL)
+		texManager.bindTexture(texManager.getResourceLocation(stack.getItemSpriteNumber))
+		TextureUtil.func_147945_b
+
+		GL11.glPopMatrix
+	}
+}
---
+++ src/main/scala/cpup/mc/testMod/content/ItemDagger.scala
@@ -0,0 +1,15 @@
+package cpup.mc.testMod.content
+
+import net.minecraft.item.{Item, ItemSword}
+import net.minecraft.item.Item.ToolMaterial
+import com.google.common.collect.{HashMultimap, Multimap}
+import net.minecraft.entity.SharedMonsterAttributes
+import net.minecraft.entity.ai.attributes.AttributeModifier
+
+class ItemDagger extends ItemSword(ToolMaterial.EMERALD) with TItemBase {
+	override def getItemAttributeModifiers = {
+		val attributes = HashMultimap.create.asInstanceOf[Multimap[String, AttributeModifier]]
+		attributes.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName, new AttributeModifier(Item.field_111210_e, "Weapon modifier", 12, 0))
+		attributes
+	}
+}
