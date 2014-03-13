a0c3791463144d07154053dcc14783dbf611b585
CoderPuppy <cpup@cpup-laptop>
2014-03-13 12:36:37 -0400

    Stop adding a fixed amount of shields and padded boots negate fall damage

--- src/main/scala/cpup/mc/testMod/content/ItemRangerArmor.scala
+++ src/main/scala/cpup/mc/testMod/content/ItemRangerArmor.scala
@@ -12,10 +12,16 @@ class ItemRangerArmor(armorType: Int) extends ItemArmor(ArmorMaterial.CHAIN, 0, 
 	override def isValidArmor(stack: ItemStack, armorType: Int, entity: Entity) = armorType == this.armorType
 
 	def damageArmor(entity: EntityLivingBase, stack: ItemStack, source: DamageSource, dmg: Int, slot: Int) {}
-	def getArmorDisplay(player: EntityPlayer, stack: ItemStack, slot: Int) = ArmorMaterial.CHAIN.getDamageReductionAmount(armorType)
+	def getArmorDisplay(player: EntityPlayer, stack: ItemStack, slot: Int) = 4
 	def getProperties(entity: EntityLivingBase, stack: ItemStack, source: DamageSource, dmg: Double, slot: Int) = {
-		new ArmorProperties(0, ArmorMaterial.CHAIN.getDamageReductionAmount(armorType), Int.MaxValue)
+		if(armorType == 3 && source == DamageSource.fall) {
+			new ArmorProperties(0, 2, Int.MaxValue)
+		} else {
+			new ArmorProperties(0, ArmorMaterial.CLOTH.getDamageReductionAmount(armorType) / 25.0, Int.MaxValue)
+		}
 	}
+
+	override def getItemEnchantability = 30
 
 	override def getArmorTexture(stack: ItemStack, entity: Entity, slot: Int, layer: String) = {
 		mod.ref.modID + ":textures/armors/ranger." + layer + ".png"
